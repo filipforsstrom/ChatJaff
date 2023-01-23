5308ba1fc4b6956970c11b56e138d9ab8f1bae1a
alexurtti <alexurtti@hotmail.com>
2023-01-23 15:50:57 +0100

    added models to backend createChat

---
+++ ChatJaffApp/ChatJaffApp/Server/Chat/CreateChat/Models/ChatMembersDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatJaffApp.Server.Chat.CreateChat.Models
+{
+    public class ChatMembersDTO
+    {
+        public string? UserId { get; set; }
+        public string? Username { get; set; }
+    }
+}
