4055411f8c8b5c87a7a196ab7c1decb2b9b7df16
alexurtti <alexurtti@hotmail.com>
2023-01-23 15:50:41 +0100

    added createChat controller

---
+++ ChatJaffApp/ChatJaffApp/Server/Chat/CreateChat/Controllers/CreateChatController.cs
@@ -0,0 +1,19 @@
+﻿using ChatJaffApp.Client.Chat.CreateChat.Models;
+using ChatJaffApp.Server.Chat.CreateChat.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatJaffApp.Server.Chat.CreateChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateChatController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> CreateChat(CreateChatDTO chatRequest)
+        {
+            return null;
+        }
+    }
+}
