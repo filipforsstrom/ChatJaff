086a0c65186349b44c1fee5339d27f0ca537759f
alexurtti <alexurtti@hotmail.com>
2023-01-23 14:21:45 +0100

    Added models to CreateChat

---
+++ ChatJaffApp/ChatJaffApp/Client/Chat/CreateChat/Models/InviteMemberRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatJaffApp.Client.Chat.CreateChat.Models
+{
+    public class InviteMemberRequest
+    {
+        [Required]
+        [MinLength(3)]
+        public string? SearchedUsername { get; set; } = string.Empty;
+    }
+}
