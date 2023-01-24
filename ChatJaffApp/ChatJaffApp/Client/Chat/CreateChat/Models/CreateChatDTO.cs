2b9066c18a74f35441a46fe56a83cafdf6f7e17b
alexurtti <alexurtti@hotmail.com>
2023-01-24 11:52:15 +0100

    createchatdto

---
+++ ChatJaffApp/ChatJaffApp/Client/Chat/CreateChat/Models/CreateChatDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatJaffApp.Client.Chat.CreateChat.Models
+{
+    public class CreateChatDTO
+    {
+        public bool Encrypted { get; set; }
+        public string? ChatName { get; set; }
+        public List<ChatMember>? Chatmembers { get; set; }
+    }
+}
