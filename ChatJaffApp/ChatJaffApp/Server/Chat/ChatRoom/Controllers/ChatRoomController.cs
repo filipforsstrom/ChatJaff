8e87690d710cad8a8dbc98b273df0e3bf4b789b7
alexurtti <alexurtti@hotmail.com>
2023-01-24 11:54:59 +0100

    chatroomcontroller

---
+++ ChatJaffApp/ChatJaffApp/Server/Chat/ChatRoom/Controllers/ChatRoomController.cs
@@ -0,0 +1,18 @@
+﻿using ChatJaffApp.Server.Chat.ChatRoom.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatJaffApp.Server.Chat.ChatRoom.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatRoomController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> CreateChat(CreateChatDTO chatRequest)
+        {
+            return Ok();
+        }
+    }
+}
