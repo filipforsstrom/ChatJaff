0220dd80a77f042aa15ac34827c625b44c8b154b
alexurtti <alexurtti@hotmail.com>
2023-01-24 11:53:43 +0100

    created imemberservice on client

---
+++ ChatJaffApp/ChatJaffApp/Client/Chat/Member/Contracts/IMemberService.cs
@@ -0,0 +1,10 @@
+﻿using ChatJaffApp.Client.Chat.CreateChat.Models;
+using ChatJaffApp.Client.Chat.Member.Models;
+
+namespace ChatJaffApp.Client.Chat.Member.Contracts
+{
+    public interface IMemberService
+    {
+        Task<InviteMemberResponse> AddMemberToChat(InviteMemberDTO invMemberDto);
+    }
+}
