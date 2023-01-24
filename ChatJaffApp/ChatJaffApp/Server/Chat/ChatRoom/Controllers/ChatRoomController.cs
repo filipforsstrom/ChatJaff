97c3dcd88f61c98c7a09041e45069c08d48dc0c5
alexurtti <alexurtti@hotmail.com>
2023-01-24 15:12:58 +0100

    added functionallity to controller for a mocked post request

--- ChatJaffApp/ChatJaffApp/Server/Chat/ChatRoom/Controllers/ChatRoomController.cs
+++ ChatJaffApp/ChatJaffApp/Server/Chat/ChatRoom/Controllers/ChatRoomController.cs
@@ -8,11 +8,42 @@ namespace ChatJaffApp.Server.Chat.ChatRoom.Controllers
     [ApiController]
     public class ChatRoomController : ControllerBase
     {
+        //fake db
+        public static List<MockChatModel> ChatModel { get; set; } = new();
+
         [HttpPost]
         [Route("[action]")]
         public async Task<IActionResult> CreateChat(CreateChatDTO chatRequest)
         {
-            return Ok();
+            MockChatModel newChat = new MockChatModel()
+            {
+                Id = 23,
+                Encrypted = chatRequest.Encrypted,
+                ChatName = chatRequest.ChatName,
+
+            };
+
+            foreach (var member in chatRequest.Chatmembers)
+            {
+                newChat.Members.Add(member.Username);
+            }
+
+            ChatModel.Add(newChat);
+
+
+            return Ok(newChat.Id);
+        }
+
+
+       
+        public class MockChatModel
+        {
+            public int Id { get; set; }
+            public List<string> Members { get; set; } = new();
+            public string? Creator { get; set; } 
+            public List<string> Messages { get; set; } = new();
+            public bool Encrypted { get; set; }
+            public string? ChatName { get; set; }
         }
     }
 }
