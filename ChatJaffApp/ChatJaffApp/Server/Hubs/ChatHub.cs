0c37d5dbe86c18ecefcf3094f60725b93a6fd61a
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2023-01-27 11:32:33 +0100

    Chatmembers can now communicate in secluded groups.

--- ChatJaffApp/ChatJaffApp/Server/Hubs/ChatHub.cs
+++ ChatJaffApp/ChatJaffApp/Server/Hubs/ChatHub.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChatJaffApp.Client.ChatRoom.Pages;
+using Microsoft.AspNetCore.SignalR;
 using System;
 namespace ChatJaffApp.Server.Hubs
 {
 	public class ChatHub : Hub
 	{
-        public async Task SendMessageAsync(string userName,string message)
+        
+        public async Task SendMessageAsync(string userName,string message, Guid chatroomId)
         {
-            
-            await Clients.All.SendAsync("ReceiveMessage", message, userName);
-            
+
+            await Clients.Groups(chatroomId.ToString()).SendAsync("ReceiveMessage", userName, message);
+        }
+
+        public async Task AddToGroup(string chatRoomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatRoomId);
+
+            var group = Groups;
+
+            await Clients.Group(chatRoomId).SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined the group {chatRoomId}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
         }
         //public async Task ChatNotificationAsync(string message, string receiverUserId, string senderUserId)
         //{
