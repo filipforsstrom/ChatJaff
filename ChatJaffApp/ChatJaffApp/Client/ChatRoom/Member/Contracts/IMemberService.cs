1ed10e0c70acbb0c4033ebb224ca5100994aa830
jsprolsson <jesper.p.olsson@hotmail.com>
2023-01-30 23:37:20 +0100

    name corrections and addmember func in service

--- ChatJaffApp/ChatJaffApp/Client/ChatRoom/Member/Contracts/IMemberService.cs
+++ ChatJaffApp/ChatJaffApp/Client/ChatRoom/Member/Contracts/IMemberService.cs
@@ -1,10 +1,12 @@
 ﻿using ChatJaffApp.Client.ChatRoom.CreateChat.Models;
 using ChatJaffApp.Client.ChatRoom.Member.Models;
+using ChatJaffApp.Client.Shared.Models;
 
 namespace ChatJaffApp.Client.ChatRoom.Member.Contracts
 {
     public interface IMemberService
     {
-        Task<InviteMemberResponse> AddMemberToChat(InviteMemberDto invMemberDto);
+        Task<ServiceResponseViewModel<ChatMemberResponse>> GetChatMember(InviteMemberDto invMemberDto);
+        Task<ServiceResponseViewModel<ChatMemberResponse>> AddChatMember(AddMemberDto invMemberDto);
     }
 }
