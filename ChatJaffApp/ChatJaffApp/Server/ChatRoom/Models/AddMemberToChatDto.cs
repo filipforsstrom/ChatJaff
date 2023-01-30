f5980c18a190917997bf28e2f17a9e0635e962c5
jsprolsson <jesper.p.olsson@hotmail.com>
2023-01-30 23:40:38 +0100

    name changes (get instead of add) and dto for server

---
+++ ChatJaffApp/ChatJaffApp/Server/ChatRoom/Models/AddMemberToChatDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatJaffApp.Server.ChatRoom.Models
+{
+    public class AddMemberToChatDto
+    {
+        public Guid UserId { get; set; }
+        public Guid? ChatId { get; set; }
+    }
+}
