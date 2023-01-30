502414d54624b994ed3f8819ec974b9487314468
jsprolsson <jesper.p.olsson@hotmail.com>
2023-01-30 23:39:18 +0100

    add memberId to list of users in chat in controller and repo

--- ChatJaffApp/ChatJaffApp/Server/ChatRoom/Contracts/IChatRoomRepository.cs
+++ ChatJaffApp/ChatJaffApp/Server/ChatRoom/Contracts/IChatRoomRepository.cs
@@ -1,8 +1,11 @@
-﻿namespace ChatJaffApp.Server.ChatRoom.Contracts
+﻿using ChatJaffApp.Server.ChatRoom.Models;
+
+namespace ChatJaffApp.Server.ChatRoom.Contracts
 {
     public interface IChatRoomRepository
     {
         Task<Guid> CreateChatRoomAsync(IChat chatRoom);
         IEnumerable<IChat> GetAllChatRooms();
+        bool AddMemberToChat(AddMemberToChatDto member);
     }
 }
