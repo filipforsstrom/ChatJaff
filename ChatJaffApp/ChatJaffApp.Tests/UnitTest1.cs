7920e916ab64485eb3702f54043d5850d02bb935
alexurtti <alexurtti@hotmail.com>
2023-01-31 14:56:50 +0100

    creted a first test in test project

--- ChatJaffApp/ChatJaffApp.Tests/UnitTest1.cs
+++ ChatJaffApp/ChatJaffApp.Tests/UnitTest1.cs
@@ -10,25 +10,58 @@ namespace ChattJaffApp.Tests;
 [TestFixture]
 public class Tests : PageTest
 {
+    //[Test]
+    //public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    //{
+    //    int nr = 6;
+    //    await Page.GotoAsync("https://playwright.dev");
+
+    //    // Expect a title "to contain" a substring.
+    //    await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+    //    // create a locator
+    //    var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+
+    //    // Expect an attribute "to be strictly equal" to the value.
+    //    await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+    //    // Click the get started link.
+    //    await getStarted.ClickAsync();
+
+    //    // Expects the URL to contain intro.
+    //    await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+    //}
+
     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    public async Task LoginUserFromHomePage_ExpektToSeeLougOutButton()
     {
-        int nr = 6;
-        await Page.GotoAsync("https://playwright.dev");
+        //go to homepage
+        await Page.GotoAsync("https://localhost:7085/");
 
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+        //expect hello world text
+        await Expect(Page.GetByText("Hello, world!")).ToBeVisibleAsync();
 
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+        //click on login link in nav menu
+        await Page.GetByTestId("login-link").ClickAsync();
 
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+        //expect to be on login page
+        await Expect(Page).ToHaveURLAsync("https://localhost:7085/account/login");
 
-        // Click the get started link.
-        await getStarted.ClickAsync();
+        //fill form with user email
+        await Page.GetByTestId("login-input-form").FillAsync("member2@gmail.com");
 
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+        //fill form with user password
+       await Page.GetByTestId("password-input-form").FillAsync("member2");
+
+        //click on login button
+        await Page.GetByTestId("login-button").ClickAsync();
+
+
+        //expect to see logout link in navmenu
+        await Expect(Page.GetByTestId("logout-link")).ToBeVisibleAsync();
+
+
+
     }
+
 }
