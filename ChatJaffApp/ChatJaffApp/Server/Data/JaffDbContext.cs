035f245b91d54b98eb6fbf6c0cef2d4297c66216
filipforsstrom <filipforsstrom@gmail.com>
2023-02-03 15:51:58 +0100

    member1 and member2 in jaffdb

--- ChatJaffApp/ChatJaffApp/Server/Data/JaffDbContext.cs
+++ ChatJaffApp/ChatJaffApp/Server/Data/JaffDbContext.cs
@@ -5,40 +5,40 @@ using ChatJaffApp.Server.Data.Models;
 
 namespace ChatJaffApp.Server.Data
 {
-	public class JaffDbContext : DbContext
-	{
-		public DbSet<Chat> ChatRooms { get; set; }
-		public DbSet<Message> Messages { get; set; }
+    public class JaffDbContext : DbContext
+    {
+        public DbSet<Chat> ChatRooms { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
-		public DbSet<ChatMember> ChatMembers { get; set; }
+        public DbSet<ChatMember> ChatMembers { get; set; }
 
-		public DbSet<Member> Members { get; set; }
+        public DbSet<Member> Members { get; set; }
 
-		public JaffDbContext(DbContextOptions<JaffDbContext> options) : base(options)
-		{
+        public JaffDbContext(DbContextOptions<JaffDbContext> options) : base(options)
+        {
 
-		}
+        }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			SeedChatRooms(modelBuilder);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SeedChatRooms(modelBuilder);
             SeedUsers(modelBuilder);
             SeedChatMembers(modelBuilder);
-            SeedMessages(modelBuilder); 
+            SeedMessages(modelBuilder);
 
             modelBuilder.Entity<ChatMember>().HasKey(cm => new { cm.ChatId, cm.UserId });
-		}
+        }
 
         private void SeedChatRooms(ModelBuilder modelBuilder)
         {
-			List<Chat> chats = new List<Chat>()
-			{
-				new Chat() {
-					Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
-					ChatName = "Chat 1",
-					Creator = "Filip",
-					Encrypted= false,
-				},
+            List<Chat> chats = new List<Chat>()
+            {
+                new Chat() {
+                    Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+                    ChatName = "Chat 1",
+                    Creator = "Filip",
+                    Encrypted= false,
+                },
                 new Chat() {
                     Id = Guid.Parse("00000000-0000-0000-0000-000000000002"),
                     ChatName = "Chat 2",
@@ -47,19 +47,21 @@ namespace ChatJaffApp.Server.Data
                 },
             };
 
-			modelBuilder.Entity<Chat>().HasData(chats);
+            modelBuilder.Entity<Chat>().HasData(chats);
         }
 
-		private void SeedUsers(ModelBuilder modelBuilder)
-		{
-			List<Member> members = new()
-			{
-				new Member () { Id = Guid.Parse("9F7E8005-873A-489D-B569-AFB17A58B051"), UserName = "Batman42"},
+        private void SeedUsers(ModelBuilder modelBuilder)
+        {
+            List<Member> members = new()
+            {
+                new Member () { Id = Guid.Parse("9CEAA7AB-1C67-4ED4-A86A-6BD01DF6C310"), UserName = "member1"},
+                new Member () { Id = Guid.Parse("b8381d75-d110-42f9-85e5-9c92a062fbc8"), UserName = "member2"},
+                new Member () { Id = Guid.Parse("9F7E8005-873A-489D-B569-AFB17A58B051"), UserName = "Batman42"},
                 new Member () { Id = Guid.Parse("C4423797-979C-4A72-A7F8-53499AAB5469"), UserName = "Catwoman"},
                 new Member () { Id = Guid.Parse("{28E44613-4ACE-474B-965F-E589E72AAF46}"), UserName = "Randy"},
                 new Member () { Id = Guid.Parse("{E36D119F-F8E3-4BD1-A3F1-0845F8CBE529}"), UserName = "Bandy"},
                 new Member () { Id = Guid.Parse("{95A471EE-E8A7-44D4-BB13-C5090FB1A5CE}"), UserName = "Silvio"},
-			};
+            };
 
             modelBuilder.Entity<Member>().HasData(members);
         }
@@ -72,7 +74,7 @@ namespace ChatJaffApp.Server.Data
                     UserId = Guid.Parse("9F7E8005-873A-489D-B569-AFB17A58B051") ,
                     ChatId= Guid.Parse("00000000-0000-0000-0000-000000000001"),
                     Content = " Cathy, Meet me at the batcave girrl"
-                    
+
                 },
                 new Message() {
                     Id = Guid.Parse("{439F95CC-B2D6-4375-9281-6A5C65BA3806}"),
@@ -80,7 +82,7 @@ namespace ChatJaffApp.Server.Data
                     ChatId= Guid.Parse("00000000-0000-0000-0000-000000000001"),
                     Content = " Sure thang battyboy"
                 }
-               
+
             };
 
             modelBuilder.Entity<Message>().HasData(messages);
@@ -89,9 +91,9 @@ namespace ChatJaffApp.Server.Data
 
         private void SeedChatMembers(ModelBuilder modelBuilder)
         {
-			List<ChatMember> chatMembers = new()
-			{
-				new ChatMember() { UserId = Guid.Parse("9F7E8005-873A-489D-B569-AFB17A58B051"), ChatId = Guid.Parse("00000000-0000-0000-0000-000000000001") },
+            List<ChatMember> chatMembers = new()
+            {
+                new ChatMember() { UserId = Guid.Parse("9F7E8005-873A-489D-B569-AFB17A58B051"), ChatId = Guid.Parse("00000000-0000-0000-0000-000000000001") },
                 new ChatMember() { UserId = Guid.Parse("C4423797-979C-4A72-A7F8-53499AAB5469"), ChatId = Guid.Parse("00000000-0000-0000-0000-000000000001") },
                 new ChatMember() { UserId =  Guid.Parse("{28E44613-4ACE-474B-965F-E589E72AAF46}"), ChatId = Guid.Parse("00000000-0000-0000-0000-000000000001") },
                 new ChatMember() { UserId =  Guid.Parse("{E36D119F-F8E3-4BD1-A3F1-0845F8CBE529}"), ChatId = Guid.Parse("00000000-0000-0000-0000-000000000002") },
