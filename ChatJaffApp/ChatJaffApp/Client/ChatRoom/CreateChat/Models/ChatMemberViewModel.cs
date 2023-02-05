977b1de007a12a0025dcdfbe3a27f9999e1ce820
jsprolsson <jesper.p.olsson@hotmail.com>
2023-02-05 03:57:46 +0100

    name change

---
+++ ChatJaffApp/ChatJaffApp/Client/ChatRoom/CreateChat/Models/ChatMemberViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatJaffApp.Client.ChatRoom.CreateChat.Models
+{
+    public class ChatMemberViewModel
+    {
+        public Guid UserId { get; set; }
+        public string? Username { get; set; }
+    }
+}
