c994a298422d219e772d3ef0d775a91d15c8f3bb
jsprolsson <jesper.p.olsson@hotmail.com>
2023-02-05 04:00:18 +0100

    name change, removed non-working auto mapping

--- ChatJaffApp/ChatJaffApp/Server/ChatRoom/Models/AddMemberToChatDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChatJaffApp.Server.ChatRoom.Models
-{
-    public class AddMemberToChatDto
-    {
-        public Guid UserId { get; set; }
-        public Guid? ChatId { get; set; }
-    }
-}
