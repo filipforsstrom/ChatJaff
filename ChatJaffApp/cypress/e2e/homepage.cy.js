f1e48a4f31d430d0748967eeec26cae309d64485
alexurtti <alexurtti@hotmail.com>
2023-02-06 13:55:39 +0100

    change method again

--- ChatJaffApp/cypress/e2e/homepage.cy.js
+++ ChatJaffApp/cypress/e2e/homepage.cy.js
@@ -1,18 +1,7 @@
 const baseUrl = "http://localhost:5172/";
-describe("test", function () {
-  // beforeEach(function () {
-  //   cy.visit("https://localhost:7085/");
-  // });
-
-  it("front page can be opened", function () {
+describe("Homepage", () => {
+  it("can be visited", () => {
     cy.visit(baseUrl);
-    cy.contains("Welcome to your new app.");
     cy.contains("Hello, world!");
   });
-
-  //   after(() => {
-  //     fetch(`${baseUrl}api/identity/kill`, {
-  //       method: "DELETE",
-  //     });
-  //   });
 });
