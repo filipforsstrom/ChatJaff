cf095794376c9fe7ef817d57e2f9f1f3233a316f
alexurtti <alexurtti@hotmail.com>
2023-02-06 14:41:27 +0100

    test 74

--- ChatJaffApp/cypress/e2e/homepage.cy.js
+++ ChatJaffApp/cypress/e2e/homepage.cy.js
@@ -2,7 +2,12 @@ const baseUrl = "http://localhost:5172/";
 
 describe("Homepage", () => {
   it("can be visited", () => {
-    cy.wait(10000);
+    cy.wait(5000);
     cy.visit(baseUrl);
-  })
-})
+  });
+
+  it("front page can be opened", function () {
+    cy.visit(baseUrl);
+    cy.contains("Hello, world!");
+  });
+});
