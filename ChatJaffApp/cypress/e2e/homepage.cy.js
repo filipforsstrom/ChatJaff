a7e6f8c9f23b069076d060df78df7b6bb069b3e0
alexurtti <alexurtti@hotmail.com>
2023-02-06 13:58:09 +0100

    added delay to first test

--- ChatJaffApp/cypress/e2e/homepage.cy.js
+++ ChatJaffApp/cypress/e2e/homepage.cy.js
@@ -1,6 +1,7 @@
 const baseUrl = "http://localhost:5172/";
 describe("Homepage", () => {
   it("can be visited", () => {
+    cy.wait(10000);
     cy.visit(baseUrl);
     cy.contains("Hello, world!");
   });
--- ChatJaffApp/cypress/e2e/login.cy.js
+++ ChatJaffApp/cypress/e2e/login.cy.js
@@ -2,7 +2,6 @@ const baseUrl = "http://localhost:5172/";
 
 describe("Homepage", () => {
   it("can be visited", () => {
-    cy.wait(10000);
     cy.visit(baseUrl);
   });
 
