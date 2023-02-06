24cd7e8b07dbbd7eac246ec9153bb39241cefe8a
alexurtti <alexurtti@hotmail.com>
2023-02-06 13:52:15 +0100

    change adress localhost

--- ChatJaffApp/cypress/e2e/homepage.cy.js
+++ ChatJaffApp/cypress/e2e/homepage.cy.js
@@ -1,18 +1,18 @@
-
+const baseUrl = "http://localhost:5172/";
 describe("test", function () {
   // beforeEach(function () {
   //   cy.visit("https://localhost:7085/");
   // });
 
   it("front page can be opened", function () {
-    cy.visit("https://localhost:7085/");
+    cy.visit(baseUrl);
     cy.contains("Welcome to your new app.");
     cy.contains("Hello, world!");
   });
 
-//   after(() => {
-//     fetch(`${baseUrl}api/identity/kill`, {
-//       method: "DELETE",
-//     });
-//   });
+  //   after(() => {
+  //     fetch(`${baseUrl}api/identity/kill`, {
+  //       method: "DELETE",
+  //     });
+  //   });
 });
