77a2cb311b31d0ad6a4111cb976515081b9af11b
alexurtti <alexurtti@hotmail.com>
2023-02-08 14:59:51 +0100

    added delete message in messages interface and service

--- ChatJaffApp/ChatJaffApp/Client/ChatRoom/Messages/Contracts/IMessageService.cs
+++ ChatJaffApp/ChatJaffApp/Client/ChatRoom/Messages/Contracts/IMessageService.cs
@@ -1,9 +1,12 @@
-﻿namespace ChatJaffApp.Client.ChatRoom.Messages.Contracts
+﻿using ChatJaffApp.Client.Shared.Models;
+
+namespace ChatJaffApp.Client.ChatRoom.Messages.Contracts
 {
     public interface IMessageService
     {
         Task SendMessage();
         Task GetChatMessages();
+        Task<ServiceResponseViewModel<string>> DeleteMessage(Guid id);
 
     }
 }
