b62246f1fcb594fe0b75d2b26a9a5632df7c218e
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2023-02-08 15:26:17 +0100

    Update EncryptionManagerTest.cs

--- ChatJaffApp/ChatJaffApi.Tests/UnitTests/EncryptionManagerTest.cs
+++ ChatJaffApp/ChatJaffApi.Tests/UnitTests/EncryptionManagerTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,20 @@ namespace ChatJaffApi.Tests.UnitTests
 {
     public class EncryptionManagerTest
     {
+        private string key { get; set; }
+        private byte[] salt { get; set; }
+        private string message { get; set; }
+
+        public EncryptionManagerTest()
+        {
+            key = "master key";
+            salt = Encoding.Unicode.GetBytes("banana");
+            message = "secret message";
+        }
+
         [Fact]
         public void EncryptMessage_OnSuccess_ReturnsDifferentString()
-        {
-            var key = "master key";
-            var salt = Encoding.Unicode.GetBytes("banana");
-            var message = "secret message";
+        {            
 
             var encryptedMsg = AesEncryptManager.Encrypt(message, key, salt);
 
@@ -23,17 +32,38 @@ namespace ChatJaffApi.Tests.UnitTests
 
         [Fact]
         public void DecryptMessage_WithCorrectParams_ReturnsSameString()
-        {
-            var key = "master key";
-            var salt = Encoding.Unicode.GetBytes("banana");
-            var message = "secret message";
-
+        {            
             var encryptedMsg = AesEncryptManager.Encrypt(message, key, salt);
             
             var decryptedMessage = AesEncryptManager.Decrypt(encryptedMsg, key, salt);
 
             
             Assert.Equal(decryptedMessage, message);
+        }
+
+        [Fact]
+        public void DecryptMessage_WithWrongKey_ThrowsException()
+        {
+            var originalKey = key;
+            var wrongKey = "wrong key";
+
+            var encryptedMsg = AesEncryptManager.Encrypt(message, originalKey, salt);
+
+            Func<string> act = () => AesEncryptManager.Decrypt(encryptedMsg, wrongKey, salt);
+
+            Assert.Throws<CryptographicException>(act);
+        }
+
+        [Fact]
+        public void DecryptMessage_WithWrongSalt_ThrowsException()
+        {            
+            var wrongSalt = Encoding.Unicode.GetBytes("oranges");
+
+            var encryptedMsg = AesEncryptManager.Encrypt(message, key, salt);
+
+            Func<string> act = () => AesEncryptManager.Decrypt(encryptedMsg, key, wrongSalt);
+
+            Assert.Throws<CryptographicException>(act);
         }
     }
 }
