36ee6c4156e033ef9c6d84028e2addc5bdda5965
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2023-02-08 11:25:54 +0100

    Created encryptionmanager for encrypt and decrypt

---
+++ ChatJaffApp/ChatJaffApp/Server/Data/Models/ChatKey.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatJaffApp.Server.Data.Models
+{
+    public class ChatKey
+    {
+        [Key]
+        public Guid ChatRoomId { get; set; }
+        [Required]
+        public string? Key { get; set; }
+    }
+}
--- ChatJaffApp/ChatJaffApp/Server/Hubs/ChatHub.cs
+++ ChatJaffApp/ChatJaffApp/Server/Hubs/ChatHub.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Text.Json;
 using ChatJaffApp.Server.ChatRoom.Contracts;
+using System.Text;
+using ChatJaffApp.Server.ChatRoom.Encryption;
 
 namespace ChatJaffApp.Server.Hubs
 {
@@ -26,15 +28,34 @@ namespace ChatJaffApp.Server.Hubs
             deserializedMessage.Sent = DateTime.UtcNow;
             deserializedMessage.ChatroomId = chatroomId;
 
-            //save to db?
             var messageToStore = _mapper.Map<Message>(deserializedMessage);
             messageToStore.ChatId = chatroomId;
 
-            await _messageRepository.AddMessageAsync(messageToStore);
+            // if encrypted chat, encrypt message
+            if (false)
+            {
+                // skapa nyckel
+                string key = "123";
+                // skapa salt
+                byte[] salt = Encoding.Unicode.GetBytes("MULLVAD");
+                // spara nyckel + iv
+                string keyToSaveInDb = $"{key}.{Convert.ToBase64String(salt)}";
+                // kryptera med aes
+                try
+                {
+                    messageToStore.Content = AesEncryptManager.Encrypt(messageToStore.Content, key, salt);
+                }
+                catch (Exception)
+                {
+                    await Clients.Groups(chatroomId.ToString()).SendAsync("ReceiveMessage", JsonSerializer.Serialize(deserializedMessage));
+                    return;
+                }
+            }
 
             var serializedResponse = JsonSerializer.Serialize(deserializedMessage);
             await Clients.Groups(chatroomId.ToString()).SendAsync("ReceiveMessage", serializedResponse);
-
+            await _messageRepository.AddMessageAsync(messageToStore);
+            deserializedMessage.Content = messageToStore.Content;
         }
 
         public async Task AddToGroup(string chatRoomId)
