6730893129dfab227685bd5593683db9bc7a1f07
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2023-02-08 15:00:29 +0100

    Created unit tests for encryption manager

---
+++ ChatJaffApp/ChatJaffApi.Tests/UnitTests/EncryptionManagerTest.cs
@@ -0,0 +1,39 @@
+﻿using ChatJaffApp.Server.ChatRoom.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatJaffApi.Tests.UnitTests
+{
+    public class EncryptionManagerTest
+    {
+        [Fact]
+        public void EncryptMessage_OnSuccess_ReturnsDifferentString()
+        {
+            var key = "master key";
+            var salt = Encoding.Unicode.GetBytes("banana");
+            var message = "secret message";
+
+            var encryptedMsg = AesEncryptManager.Encrypt(message, key, salt);
+
+            Assert.NotEqual(message, encryptedMsg);
+        }
+
+        [Fact]
+        public void DecryptMessage_WithCorrectParams_ReturnsSameString()
+        {
+            var key = "master key";
+            var salt = Encoding.Unicode.GetBytes("banana");
+            var message = "secret message";
+
+            var encryptedMsg = AesEncryptManager.Encrypt(message, key, salt);
+            
+            var decryptedMessage = AesEncryptManager.Decrypt(encryptedMsg, key, salt);
+
+            
+            Assert.Equal(decryptedMessage, message);
+        }
+    }
+}
