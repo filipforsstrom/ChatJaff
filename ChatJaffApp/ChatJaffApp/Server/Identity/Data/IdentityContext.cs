9989dfe9d0342224f607f6ae4bc3cb7734ecea66
filipforsstrom <filipforsstrom@gmail.com>
2023-02-08 13:21:31 +0100

    users and roles for ban

--- ChatJaffApp/ChatJaffApp/Server/Identity/Data/IdentityContext.cs
+++ ChatJaffApp/ChatJaffApp/Server/Identity/Data/IdentityContext.cs
@@ -17,6 +17,7 @@ namespace ChatJaffApp.Server.Identity.Data
             base.OnModelCreating(builder);
             SeedUsers(builder);
             SeedRoles(builder);
+            SeedUserRoles(builder);
 
         }
         private void SeedUsers(ModelBuilder builder)
@@ -61,9 +62,22 @@ namespace ChatJaffApp.Server.Identity.Data
 
             banned1.PasswordHash = passwordHasher.HashPassword(banned1, "banned1");
 
+            ApplicationUser toBan1 = new ApplicationUser()
+            {
+                Id = "b8381d75-d110-42f9-85e5-9c92a062a111",
+                UserName = "toBan1",
+                NormalizedUserName = "TOBAN1",
+                Email = "toBan1@gmail.com",
+                NormalizedEmail = "TOBAN1@GMAIL.COM",
+                LockoutEnabled = false,
+                EmailConfirmed = true,
+                IsBanned = false
+            };
+
+            toBan1.PasswordHash = passwordHasher.HashPassword(banned1, "toban1");
             var listOfInitialUsers = new List<ApplicationUser>()
             {
-                member1, member2, banned1
+                member1, member2, banned1, toBan1
             };
             builder.Entity<ApplicationUser>().HasData(listOfInitialUsers);
         }
@@ -85,6 +99,33 @@ namespace ChatJaffApp.Server.Identity.Data
                     Name = "Moderator",
                     ConcurrencyStamp = "1",
                     NormalizedName = "MODERATOR"
+                },
+                new IdentityRole()
+                {
+                    Id = "BannedId",
+                    Name = "Banned",
+                    ConcurrencyStamp = "1",
+                    NormalizedName = "BANNED"
+                }
+                );
+        }
+        private void SeedUserRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>()
+                {
+                    RoleId = "MemberId",
+                    UserId = "9CEAA7AB-1C67-4ED4-A86A-6BD01DF6C310"
+                },
+                new IdentityUserRole<string>()
+                {
+                    RoleId = "MemberId",
+                    UserId = "b8381d75-d110-42f9-85e5-9c92a062fbc8"
+                },
+                new IdentityUserRole<string>()
+                {
+                    RoleId = "BannedId",
+                    UserId = "b8381d75-d110-42f9-85e5-9c92a062abc1"
                 }
                 );
         }
