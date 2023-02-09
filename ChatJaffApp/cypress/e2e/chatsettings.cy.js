3c15701b4a52a234eb48fab7ef6c47bbcf997c44
jsprolsson <jesper.p.olsson@hotmail.com>
2023-02-09 19:22:57 +0100

    fixed chat settings test

--- ChatJaffApp/cypress/e2e/chatsettings.cy.js
+++ ChatJaffApp/cypress/e2e/chatsettings.cy.js
@@ -7,13 +7,23 @@ describe("ChatSettings", () => {
         cy.get("#email").type("member2@gmail.com");
         cy.get("#password").type("member2");
         cy.get("#login-button").click();
-        cy.contains("Logout");
+
+      cy.contains("Chat Rooms").click();
+      cy.contains("Create new chat");
+      cy.get("#add-new-chat-button").click();
+      cy.url().should("include", "/chatRooms/createchat");
+      cy.get("#add-chat-member-button").click();
+      cy.contains("Search field must not be empty.");
+      cy.get("#search-user").type("catwoman");
+      cy.get("#add-chat-member-button").click();
+      cy.get("#chat-name").type("test chat");
+      cy.get("#create-chat-button").click();
       });
 
       it("delete chat", () => {
-        cy.visit(`${baseUrl}chatrooms/5d728ec3-1f6b-4170-8827-bc064ae25a41`);
         cy.contains("Settings").click();
         cy.contains("Delete Chat").click();
-        cy.url().should("eq", baseUrl + "chatrooms");
+        cy.contains('test chat').should('not.exist');
+        cy.contains('My Chat Rooms').should('exist');
       })
 })
