ee8663dc329d6cbd82f69cbe684659b3f642283e
jonathandepartment <68937234+jonathandepartment@users.noreply.github.com>
2023-02-10 13:07:39 +0100

    Fixed tests that broke

--- ChatJaffApp/cypress/e2e/login.cy.js
+++ ChatJaffApp/cypress/e2e/login.cy.js
@@ -1,11 +1,12 @@
 const baseUrl = "http://localhost:5172/";
 
+
 describe("Login", () => {
-  beforeEach(function () {
+  before(function () {
     cy.visit(baseUrl);
     cy.get('[data-cy="reglink"]').click();
 
-    cy.get("#register-email").type("member3@gmail.com");
+    cy.get("#register-email").type("member2000@gmail.com");
     cy.get("#register-username").type("sandy");
     cy.get("#register-password").type("Member123!");
     cy.get("#register-confirmed-password").type("Member123!");
@@ -13,13 +14,15 @@ describe("Login", () => {
   });
 
   it("logs in user successfully", function () {
-    cy.get("#email").type("member3@gmail.com");
+    cy.visit(`${baseUrl}account/login`);
+    cy.get("#email").type("member2000@gmail.com");
     cy.get("#password").type("Member123!");
     cy.get("#login-button").click();
     cy.contains("Logout");
   });
 
   it("fails when not a registered member", function () {
+    cy.visit(`${baseUrl}account/login`);
     cy.get("#email").type("member300@gmail.com");
     cy.get("#password").type("member2");
     cy.get("#login-button").click();
@@ -27,24 +30,34 @@ describe("Login", () => {
   });
 
   it("fails when password is wrong", function () {
-    cy.get("#email").type("member3@gmail.com");
+    cy.visit(`${baseUrl}account/login`);
+    cy.get("#email").type("member2000@gmail.com");
     cy.get("#password").type("member2");
     cy.get("#login-button").click();
     cy.contains("Invalid Credentials");
   });
 
-  it("locks out a user after 4 attempts", function () {
-    cy.get("#email").type("member3@gmail.com");
+  it.only("locks out a user after 4 attempts", function () {
+    cy.visit(baseUrl);
+    cy.get('[data-cy="reglink"]').click();
+
+    cy.get("#register-email").type("member391023@gmail.com");
+    cy.get("#register-username").type("megan");
+    cy.get("#register-password").type("Member123!");
+    cy.get("#register-confirmed-password").type("Member123!");
+    cy.get("#register-button").click();
+
+    cy.get("#email").type("member391023@gmail.com");
     cy.get("#password").type("member2");
 
-    var loginBtn = cy.get("#login-button");
-    loginBtn.click();
-    cy.contains("Invalid Credentials");
 
-    for (let i = 0; i < 3; i++) {
-      loginBtn.click()
+
+    for (let i = 0; i < 4; i++) {
+      cy.get("#login-button").click().then((btn) => cy.wait(1000))
+
     }
 
     cy.contains("User is locked out")
   });
+
 });
